bbfd3d50c20abc94243f10625ee156fd1a761210
zaqlul9595 <64331241+zaqlul9595@users.noreply.github.com>
2024-05-16 01:02:13 -0500

    Updated role file
    Now contains all the file paths, just needs the code

---
+++ ttt2-role_vulture/lua/terrortown/lang/en/vulture.lua
@@ -0,0 +1,18 @@
+--Role Language Strings
+L[VULTURE.name] = "Vulture"
+L[VULTURE.defaultTeam] = "Team Vulture"
+L["hilite_win_" .. VULTURE.defaultTeam] = "TEAM VULTURE WON"
+L["win_" .. VULTURE.defaultTeam] = "The Vulture has won!"
+L["info_popup_" .. VULTURE.name] = [[You are a scavenger of the skirmish. Press 'INSERT KEY HERE' while holding your vulture knife to eat bodies for health. Eat enough bodies and you win!]]
+L["body_found_" .. VULTURE.abbr] = "They were the Vulture!"
+L["search_role_" .. VULTURE.abbr] = "This person was the Vulture!"
+L["ev_win_" .. VULTURE.defaultTeam] = "The Vulture has won!"
+L["target_" .. VULTURE.name] = "Vulture"
+L["ttt2_desc_" .. VULTURE.name] = [[The Vulture needs to eat a certain number of bodies to win. They come equipped with a vulture knife that gibs other players, and is used to consume bodies for health. 
+The vulture is also able to see bodies throughout the map.]]
+
+
+--Other Role Language Strings Go Here
+-- Like when the vulture eats a body and is brodcasted to other players
+-- Or when the vulture heals from eating a body
+L["vulture_knife_name"] = "Vulture Knife"
