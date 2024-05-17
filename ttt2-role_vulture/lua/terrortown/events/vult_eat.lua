90d8c6a0a5b7b2db8e6459c0c148694743729557
milkwxter <ethan.r.bates2003@gmail.com>
2024-05-17 12:48:11 -0500

    Digestion cooldown now prints to HUD

--- ttt2-role_vulture/lua/terrortown/events/vult_eat.lua
+++ ttt2-role_vulture/lua/terrortown/events/vult_eat.lua
@@ -1,28 +1,31 @@
 if CLIENT then
     EVENT.icon = Material("vgui/ttt/dynamic/roles/icon_vult.vmt")
     EVENT.title = "EVENT_VULT_CONSUME"
+	
+	--The deputy uses some sort of status to get the timer next to a png like a gun or in this case, a vulture.
+	--But I don't think this is in normal glua, and doesn't exist outside of the source code(seriously try googling it)
+	hook.Add("Initialize", "ttt2_vult_init", function()
+		STATUS:RegisterStatus("ttt2_vult_cooldown_stat", {
+			hud = Material("vgui/ttt/icons/digesting_icon.png"),
+			type = "bad",
+			name = "Digestion cooldown",
+			sidebarDescription = "You cannot eat bodies until this cooldown is up."
+		})
+	end)
 end
 
 --Function that increases bodies consumed by 'a'
 local function incVultCounter(a)
     VULTURE_DATA.amount_eaten = VULTURE_DATA.amount_eaten + a
-    print("Current:", VULTURE_DATA.amount_eaten)
+    print("Currently you ate this many bodies:", VULTURE_DATA.amount_eaten)
     LANG.MsgAll("label_vult_consume_notification",nil,MSG_MSTACK_WARN)
 	if(VULTURE_DATA.amount_eaten >= GetConVar("ttt2_vult_consumed_bodies_win_threshold"):GetInt()) then
         roles.VULTURE.shouldWin = true
         VULTURE_DATA.amount_eaten = 0
-        print("New current:", VULTURE_DATA.amount_eaten)
+        print("Bodies eaten reset to:", VULTURE_DATA.amount_eaten)
     end
 end
 
---The deputy uses some sort of status to get the timer next to a png like a gun or in this case, a vulture.
---But I don't think this is in normal glua, and doesn't exist outside of the source code(seriously try googling it)
---hook.Add("Initialize", "ttt2_vult_init", function()
---		STATUS:RegisterStatus("ttt2_vult_cooldown_stat", {
---			hud = Material("vgui/ttt/icons/vult.png"),
---			type = "bad"
---			})
---end)
 
 -- reset hooks at round end
 hook.Add("TTTEndRound", "VultureEndRound", function()
